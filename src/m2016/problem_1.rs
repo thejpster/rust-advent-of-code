4c8c8a49a31c2024e656907898ff5c2a4e8db7f1
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-02 18:02:40 +0000

    Sheesh 2016 was hard. Finished 1b.

--- src/m2016/problem_1.rs
+++ src/m2016/problem_1.rs
@@ -12,13 +12,19 @@ enum Turn {
     Right(i32),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
 #[derive(Debug, Clone, Copy)]
-struct State(Dir, i32, i32);
+struct State(Dir, Position);
 
 pub fn run(contents: &Vec<Vec<String>>) {
     // Big one line string
     let first_line = &contents[0][0];
-    let mut state = State(Dir::Up, 0, 0);
+    let mut state = State(Dir::Up, Position { x: 0, y: 0 });
     let turns = first_line.split_whitespace().map(|x| {
         // println!("Got {}", x);
         let val = &x[1..].replace(",", "");
@@ -28,11 +34,70 @@ pub fn run(contents: &Vec<Vec<String>>) {
             _ => panic!("Bad move {}", x),
         }
     });
+    let mut visit = vec! [ ];
     for turn in turns {
-        state = state + turn
+        // println!("Turn: {:?}", turn);
+        let old_state = state.clone();
+        state = state + turn;
+        visit.extend(steps(&old_state.1, &state.1));
     }
     println!("At {:?}", state);
-    println!("Distance: {}", state.1.abs() + state.2.abs())
+    println!("Distance: {}", state.distance());
+
+    for i in 0..visit.len() {
+        for j in i + 1..visit.len() {
+            if visit[i] == visit[j] {
+                println!("I ({}) {:?}", i, visit[i]);
+                println!("J ({}) {:?}", j, visit[j]);
+                println!("Distance: {}", visit[i].distance());
+                return;
+            }
+        }
+    }
+}
+
+fn steps(pos1: &Position, pos2: &Position) -> Vec<Position> {
+    let result = if pos1.x == pos2.x {
+        // walking along y
+        if pos1.y < pos2.y {
+            // walking forwards
+            (pos1.y .. pos2.y).map(|y| Position { x: pos1.x, y }).collect()
+        } else {
+            // walking backwards
+            let mut v = (pos2.y + 1 .. pos1.y + 1).map(|y| Position { x: pos1.x, y }).collect::<Vec<_>>();
+            v.reverse();
+            v
+        }
+
+    } else {
+        // walking along x
+        if pos1.x < pos2.x {
+            // walking forwards
+            (pos1.x .. pos2.x).map(|x| Position { x, y: pos1.y }).collect()
+        } else {
+            // walking backwards
+            let mut v = (pos2.x + 1 .. pos1.x + 1).map(|x| Position { x, y: pos1.y }).collect::<Vec<_>>();
+            v.reverse();
+            v
+        }
+    };
+    result
+}
+
+impl Position {
+    fn distance(&self) -> i32 {
+        return self.x.abs() + self.y.abs();
+    }
+}
+
+impl State {
+    fn distance(&self) -> i32 {
+        return self.1.distance();
+    }
+
+    fn same_place(&self, rhs: &State) -> bool {
+        return self.1 == rhs.1;
+    }
 }
 
 impl ::std::ops::Add<Turn> for Dir {
@@ -68,10 +133,10 @@ impl ::std::ops::Add<Turn> for State {
             Turn::Right(x) => x,
         };
         match self.0 {
-            Dir::Up => self.1 = self.1 + distance,
-            Dir::Down => self.1 = self.1 - distance,
-            Dir::Left => self.2 = self.2 - distance,
-            Dir::Right => self.2 = self.2 + distance,
+            Dir::Up => self.1.x = self.1.x + distance,
+            Dir::Down => self.1.x = self.1.x - distance,
+            Dir::Left => self.1.y = self.1.y - distance,
+            Dir::Right => self.1.y = self.1.y + distance,
         }
         self
     }
