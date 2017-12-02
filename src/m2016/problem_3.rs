4ca663e2469c118e350624b6e1853191c6900a59
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-02 18:50:45 +0000

    2016.3

--- src/m2016/problem_3.rs
+++ src/m2016/problem_3.rs
@@ -1,3 +1,41 @@
-pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+pub fn run(contents: &Vec<Vec<String>>) {
+    let mut count = 0;
+    for line in &contents[0] {
+        let parts: Vec<u32> = line.split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        if possible(parts) {
+            count = count + 1;
+        }
+    }
+    println!("Count: {}", count);
+
+    count = 0;
+    for line in contents[0].chunks(3) {
+        let parts: Vec<Vec<u32>> = line.iter()
+            .map(|x| {
+                x.split_whitespace().map(|x| x.parse().unwrap()).collect()
+            })
+            .collect();
+
+        let tri1 = vec![parts[0][0], parts[1][0], parts[2][0]];
+        let tri2 = vec![parts[0][1], parts[1][1], parts[2][1]];
+        let tri3 = vec![parts[0][2], parts[1][2], parts[2][2]];
+        if possible(tri1) {
+            count = count + 1;
+        }
+        if possible(tri2) {
+            count = count + 1;
+        }
+        if possible(tri3) {
+            count = count + 1;
+        }
+    }
+
+    println!("Count: {}", count);
+}
+
+fn possible(mut v: Vec<u32>) -> bool {
+    v.sort();
+    v[0] + v[1] > v[2]
 }
