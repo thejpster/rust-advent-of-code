b931c6fdc967219ceaca08e23719c25a0a01267a
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-02 10:13:13 +0000

    Support other years.

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,12 @@ extern crate failure;
 extern crate failure_derive;
 
 mod m2017;
-// mod m2016;
+mod m2016;
 
 use std::fs::File;
 use std::io::prelude::*;
 use std::env;
 
-use m2017 as year;
 
 use failure::Error;
 
@@ -53,7 +52,7 @@ enum AppError {
     ),
     #[fail(display = "{} doesn't look like a number", _0)]
     BadNumber(String),
-    #[fail(display = "you should call '{} <problem> <file> [ <file> ... ]'", _0)]
+    #[fail(display = "you should call '{} <year> <problem> <file> [ <file> ... ]'", _0)]
     BadArgs(String),
 }
 
@@ -76,7 +75,7 @@ enum AppError {
 /// Parses the arguments, reads the input files and executes the specified
 /// problem.
 fn main() {
-    let (problem, filenames) = match parse_args() {
+    let (year, problem, filenames) = match parse_args() {
         Ok(v) => v,
         Err(e) => {
             eprintln!("Error parsing arguments: {}", e);
@@ -93,11 +92,73 @@ fn main() {
             std::process::exit(1);
         }
     };
-    match problem {
-        1 => year::problem_1::run(&files),
-        2 => year::problem_2::run(&files),
-        n => {
-            eprintln!("Don't have problem {}", n);
+    match year {
+        2016 => {
+            match problem {
+                1 => m2016::problem_1::run(&files),
+                2 => m2016::problem_2::run(&files),
+                3 => m2016::problem_3::run(&files),
+                4 => m2016::problem_4::run(&files),
+                5 => m2016::problem_5::run(&files),
+                6 => m2016::problem_6::run(&files),
+                7 => m2016::problem_7::run(&files),
+                8 => m2016::problem_8::run(&files),
+                9 => m2016::problem_9::run(&files),
+                10 => m2016::problem_10::run(&files),
+                11 => m2016::problem_11::run(&files),
+                12 => m2016::problem_12::run(&files),
+                13 => m2016::problem_13::run(&files),
+                14 => m2016::problem_14::run(&files),
+                15 => m2016::problem_15::run(&files),
+                16 => m2016::problem_16::run(&files),
+                17 => m2016::problem_17::run(&files),
+                18 => m2016::problem_18::run(&files),
+                19 => m2016::problem_19::run(&files),
+                20 => m2016::problem_20::run(&files),
+                21 => m2016::problem_21::run(&files),
+                22 => m2016::problem_22::run(&files),
+                23 => m2016::problem_23::run(&files),
+                24 => m2016::problem_24::run(&files),
+                _ => {
+                    eprintln!("Don't have problem {} in year {}", problem, year);
+                    std::process::exit(1);
+                }
+            }
+        }
+        2017 => {
+            match problem {
+                1 => m2017::problem_1::run(&files),
+                2 => m2017::problem_2::run(&files),
+                3 => m2017::problem_3::run(&files),
+                4 => m2017::problem_4::run(&files),
+                5 => m2017::problem_5::run(&files),
+                6 => m2017::problem_6::run(&files),
+                7 => m2017::problem_7::run(&files),
+                8 => m2017::problem_8::run(&files),
+                9 => m2017::problem_9::run(&files),
+                10 => m2017::problem_10::run(&files),
+                11 => m2017::problem_11::run(&files),
+                12 => m2017::problem_12::run(&files),
+                13 => m2017::problem_13::run(&files),
+                14 => m2017::problem_14::run(&files),
+                15 => m2017::problem_15::run(&files),
+                16 => m2017::problem_16::run(&files),
+                17 => m2017::problem_17::run(&files),
+                18 => m2017::problem_18::run(&files),
+                19 => m2017::problem_19::run(&files),
+                20 => m2017::problem_20::run(&files),
+                21 => m2017::problem_21::run(&files),
+                22 => m2017::problem_22::run(&files),
+                23 => m2017::problem_23::run(&files),
+                24 => m2017::problem_24::run(&files),
+                _ => {
+                    eprintln!("Don't have problem {} in year {}", problem, year);
+                    std::process::exit(1);
+                }
+            }
+        }
+        _ => {
+            eprintln!("Don't have year {}", year);
             std::process::exit(1);
         }
     }
@@ -109,17 +170,19 @@ fn main() {
 //
 // ****************************************************************************
 
-fn parse_args() -> Result<(u32, Vec<String>), Error> {
+fn parse_args() -> Result<(u32, u32, Vec<String>), Error> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
+    if args.len() < 4 {
         return Err(AppError::BadArgs(args[0].clone()).into());
     }
-    let problem_number = &args[1];
-    let file_names = args[2..].to_vec();
-    let problem_number = problem_number.parse::<u32>().map_err(|_| {
-        AppError::BadNumber(problem_number.clone())
+    let year = args[1].parse::<u32>().map_err(|_| {
+        AppError::BadNumber(args[1].clone())
     })?;
-    return Ok((problem_number, file_names));
+    let problem_number = args[2].parse::<u32>().map_err(|_| {
+        AppError::BadNumber(args[2].clone())
+    })?;
+    let file_names = args[3..].to_vec();
+    return Ok((year, problem_number, file_names));
 }
 
 fn open(filename: &str) -> Result<Vec<String>, Error> {
