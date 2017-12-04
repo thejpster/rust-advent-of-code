669a268ddb0fc994ea7bb0ed3b5ff466dee8038d
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-04 19:03:19 +0000

    Solved 2016.5

--- src/m2016/problem_5.rs
+++ src/m2016/problem_5.rs
@@ -1,3 +1,55 @@
+use md5;
+
+const DOOR_ID: &'static str = "wtnhxymk";
+// const DOOR_ID:&'static str = "abc";
+
 pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+    let mut count = 0;
+    for i in 1..99999999 {
+        // println!("i = {}", i);
+        let input = format!("{}{}", DOOR_ID, i);
+        // println!("input = {}", input);
+        let hash = md5::compute(input);
+        // println!("hash = {:x}", hash);
+        let digit = hash[2] & 0x0F;
+        if (hash[0] == 0) && (hash[1] == 0) && (hash[2] & 0xF0 == 0) {
+            println!("{:x}", digit);
+            count = count + 1;
+            if count == 8 {
+                break;
+            }
+        }
+    }
+
+    let mut count = 0;
+    let mut output: [Option<u8>; 8] = [None; 8];
+    for i in 1..99999999 {
+        // println!("i = {}", i);
+        let input = format!("{}{}", DOOR_ID, i);
+        // println!("input = {}", input);
+        let hash = md5::compute(input);
+        // println!("hash = {:x}", hash);
+        let digit = (hash[3] & 0xF0) >> 4;
+        let pos: usize = hash[2] as usize & 0x0F;
+        if (hash[0] == 0) && (hash[1] == 0) && (hash[2] & 0xF0 == 0) && (pos < 8) {
+            println!("{:x} in pos {:x}", digit, pos);
+            match output[pos] {
+                None => {
+                    output[pos] = Some(digit);
+                    count = count + 1;
+                }
+                Some(_) => {}
+            }
+            if count == 8 {
+                break;
+            }
+        }
+    }
+    println!(
+        "Output: {}",
+        output
+            .iter()
+            .map(|d| format!("{:x}", d.unwrap()))
+            .collect::<String>()
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
+extern crate md5;
 
 mod m2017;
 mod m2016;
