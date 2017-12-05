23f984d1432009680e0c6c61d95cf6e40282cd86
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-05 05:12:11 +0000

    Steps (2017.5)

--- src/m2017/problem_5.rs
+++ src/m2017/problem_5.rs
@@ -1,3 +1,27 @@
-pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+pub fn run(contents: &Vec<Vec<String>>) {
+    let mut jumps: Vec<i64> = contents[0].iter().map(|x| x.parse().unwrap()).collect();
+    let mut idx: i64 = 0;
+    let mut steps = 0;
+    while idx >= 0 && (idx as usize) < jumps.len() {
+        let old_idx = idx;
+        idx = idx + jumps[idx as usize];
+        jumps[old_idx as usize] = jumps[old_idx as usize] + 1;
+        steps = steps + 1;
+    }
+    println!("steps: {}", steps);
+
+    let mut jumps: Vec<i64> = contents[0].iter().map(|x| x.parse().unwrap()).collect();
+    let mut idx: i64 = 0;
+    let mut steps = 0;
+    while idx >= 0 && (idx as usize) < jumps.len() {
+        let old_idx = idx;
+        idx = idx + jumps[idx as usize];
+        if jumps[old_idx as usize] >= 3 {
+            jumps[old_idx as usize] = jumps[old_idx as usize] - 1;
+        } else {
+            jumps[old_idx as usize] = jumps[old_idx as usize] + 1;
+        }
+        steps = steps + 1;
+    }
+    println!("steps: {}", steps);
 }
