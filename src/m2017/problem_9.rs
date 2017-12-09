1c37358f61a7e1563cd3b90f15cc951d9c71145e
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-09 08:53:54 +0000

    2017.9 done.

--- src/m2017/problem_9.rs
+++ src/m2017/problem_9.rs
@@ -1,3 +1,32 @@
-pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+pub fn run(contents: &Vec<Vec<String>>) {
+    for line in &contents[0] {
+        let mut skip = false;
+        let mut garbage = false;
+        let mut level = 0;
+        let mut total = 0;
+        let mut garbage_count = 0;
+        for ch in line.chars() {
+            if skip {
+                skip = false;
+                continue;
+            }
+            match ch {
+                '!' if garbage => skip = true,
+                '>' if garbage => garbage = false,
+                _ if garbage => garbage_count = garbage_count + 1,
+                '{' => {
+                    level = level + 1;
+                    total = total + level;
+                }
+                '}' => {
+                    level = level - 1;
+                }
+                '<' => garbage = true,
+                '!' => panic!("Can't skip outside garbage!"),
+                '>' => panic!("Can't end garbage if it didn't start!"),
+                _ => {}
+            }
+        }
+        println!("Total: {}, Garbage Count: {}", total, garbage_count);
+    }
 }
