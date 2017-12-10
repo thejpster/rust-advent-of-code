57d07f023e00fd46f68d880405b57d09961220c9
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-10 15:39:24 +0000

    Cleaned up a bit.

--- src/m2017/problem_10.rs
+++ src/m2017/problem_10.rs
@@ -1,79 +1,50 @@
 const MAX: u8 = 255;
 
-fn run1(contents: &Vec<Vec<String>>) {
+pub fn run(contents: &Vec<Vec<String>>) {
     let mut items: Vec<u8> = (0..MAX).collect();
     // Don't have inclusive range syntax, so manually push on the last item
     items.push(MAX);
 
-    let lengths: Vec<u8> = contents[0][0]
-        .split(",")
-        .map(|x| x.parse().unwrap())
-        .collect();
-    // println!("Lengths: {:?}", items);
+    part1(items.clone(), &contents[0][0]);
+    part2(items.clone(), &contents[0][0]);
+}
 
+fn part1(mut items: Vec<u8>, contents: &str) {
+    let lengths: Vec<u8> = contents.split(",").map(|x| x.parse().unwrap()).collect();
     round(&mut items, &lengths, 1);
+    println!("{}", items[0] as u16 * items[1] as u16);
+}
 
-    println!(
-        "{} x {} = {}",
-        items[0],
-        items[1],
-        (items[0] as u32) * (items[1] as u32)
-    );
+fn part2(mut items: Vec<u8>, contents: &str) {
+    let mut lengths: Vec<u8> = contents.bytes().collect();
+    lengths.extend(&[17, 31, 73, 47, 23]);
+    round(&mut items, &lengths, 64);
+    let dense_hash: String = items[..]
+        .chunks(16)
+        .map(|c| c.iter().fold(0, |acc, x| acc ^ x))
+        .map(|x| format!("{:02x}", x))
+        .collect();
+    println!("Dense Hash: {}", dense_hash);
 }
 
 fn round(items: &mut Vec<u8>, lengths: &Vec<u8>, count: usize) {
     let mut skip: usize = 0;
     let mut start: usize = 0;
-
     for _ in 0..count {
         for x in lengths {
             let length = *x as usize;
-            // println!("****");
-            // println!("Items: {:?}", items);
-            // println!("Reversing {} from {} to {}", length, start, end);
             reverse(items, start, length);
-            // println!("Items: {:?}", items);
             start = (start + length + skip) % items.len();
-            // println!("Start: {}", start);
             skip = skip + 1;
         }
     }
 }
 
-fn run2(contents: &Vec<Vec<String>>) {
-    let mut items: Vec<u8> = (0..MAX).collect();
-    // Don't have inclusive range syntax, so manually push on the last item
-    items.push(MAX);
-
-    let mut lengths: Vec<u8> = contents[0][0].bytes().collect();
-    lengths.extend(&[17, 31, 73, 47, 23]);
-    round(&mut items, &lengths, 64);
-    let dense_hash: Vec<u8> = items[..].chunks(16).map(|c| fold(c)).collect();
-    let dh_hex: String = dense_hash.iter().map(|x| format!("{:02x}", x)).collect();
-    println!("Dense Hash: {}", dh_hex);
-}
-
-fn fold(items: &[u8]) -> u8 {
-    let mut result = 0u8;
-    for i in items {
-        result = result ^ i;
-    }
-    result
-}
-
-pub fn run(contents: &Vec<Vec<String>>) {
-    run1(contents);
-    run2(contents);
-}
-
-fn reverse(vec: &mut Vec<u8>, mut start: usize, mut length: usize) {
+fn reverse(items: &mut Vec<u8>, mut start: usize, mut length: usize) {
     while length >= 2 {
-        let end = (start + length - 1) % vec.len();
-        // println!("Swapping #{} {} with #{} {}", start, vec[start], end, vec[end]);
-        let temp = vec[end];
-        vec[end] = vec[start];
-        vec[start] = temp;
-        start = (start + 1) % vec.len();
+        let end = (start + length - 1) % items.len();
+        items.swap(start, end);
+        start = (start + 1) % items.len();
         length = length - 2;
     }
 }
