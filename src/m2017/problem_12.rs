e985d63a3fa8b1c1494aeb35c9af7dd756fca148
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-12 06:12:54 +0000

    2017.12 done.

--- src/m2017/problem_12.rs
+++ src/m2017/problem_12.rs
@@ -1,3 +1,60 @@
-pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::hash_map::Entry;
+
+pub fn run(contents: &Vec<Vec<String>>) {
+    let mut hm: HashMap<u32, Vec<u32>> = HashMap::new();
+    for line in &contents[0] {
+        let mut parts = line.split(" <-> ");
+        let primary: u32 = parts.next().unwrap().parse().unwrap();
+        let secondary: Vec<u32> = parts
+            .next()
+            .unwrap()
+            .split(", ")
+            .map(|x| x.parse().unwrap())
+            .collect();
+        match hm.entry(primary) {
+            Entry::Vacant(e) => {
+                e.insert(secondary);
+            }
+            Entry::Occupied(mut e) => {
+                e.get_mut().extend(secondary);
+            }
+        }
+    }
+
+
+    println!("Count ({}): {}", 0, count(&hm, &mut HashSet::new(), 0));
+    let mut groups = 0;
+    while hm.len() > 0 {
+        let mut seen = HashSet::new();
+        let search = *hm.keys().next().unwrap();
+        count(&hm, &mut seen, search);
+        groups = groups + 1;
+        for x in seen {
+            hm.remove(&x);
+        }
+    }
+    println!("Groups: {}", groups);
+}
+
+fn count(hm: &HashMap<u32, Vec<u32>>, seen: &mut HashSet<u32>, index: u32) -> u32 {
+    seen.insert(index);
+    match hm.get(&index) {
+        None => {
+            panic!("Non bi-dir association!")
+        }
+        Some(x) => {
+            let mut t = 1;
+            for v in x {
+                if !seen.contains(v) {
+                    seen.insert(*v);
+                    let c = count(hm, seen, *v);
+                    t = t + c;
+                }
+            }
+            t
+        }
+    }
+
 }
