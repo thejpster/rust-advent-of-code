bb69b8dea8813d0c61145c62e215aabb465e11d9
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-15 06:50:47 +0000

    Solved 2017.15. An easy one for a change!

--- src/m2017/problem_15.rs
+++ src/m2017/problem_15.rs
@@ -1,3 +1,36 @@
 pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+    const FACTOR_A: u64 = 16807;
+    const FACTOR_B: u64 = 48271;
+    const LIMIT: u64 = 2147483647;
+
+    let mut count = 0;
+    let mut a = 873;
+    let mut b = 583;
+    for _ in 0..40_000_000 {
+        a = (a * FACTOR_A) % LIMIT;
+        b = (b * FACTOR_B) % LIMIT;
+        if (a & 0xFFFF) == (b & 0xFFFF) {
+            count = count + 1;
+        }
+    }
+    println!("Count: {}", count);
+
+    let mut count = 0;
+    let mut a = 873;
+    let mut b = 583;
+    for _ in 0..5_000_000 {
+        a = (a * FACTOR_A) % LIMIT;
+        while (a % 4) != 0 {
+            a = (a * FACTOR_A) % LIMIT;
+        }
+        b = (b * FACTOR_B) % LIMIT;
+        while (b % 8) != 0 {
+            b = (b * FACTOR_B) % LIMIT;
+        }
+        if (a & 0xFFFF) == (b & 0xFFFF) {
+            count = count + 1;
+        }
+    }
+    println!("Count: {}", count);
+
 }
