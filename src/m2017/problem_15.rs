3bf6776179d354fba77997b57ea670344f15c647
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-15 07:24:00 +0000

    Sort-of a do/while loop.

--- src/m2017/problem_15.rs
+++ src/m2017/problem_15.rs
@@ -1,3 +1,4 @@
+
 pub fn run(_contents: &Vec<Vec<String>>) {
     const FACTOR_A: u64 = 16807;
     const FACTOR_B: u64 = 48271;
@@ -19,14 +20,14 @@ pub fn run(_contents: &Vec<Vec<String>>) {
     let mut a = 873;
     let mut b = 583;
     for _ in 0..5_000_000 {
-        a = (a * FACTOR_A) % LIMIT;
-        while (a % 4) != 0 {
+        while {
             a = (a * FACTOR_A) % LIMIT;
-        }
-        b = (b * FACTOR_B) % LIMIT;
-        while (b % 8) != 0 {
+            (a % 4) != 0
+        } {}
+        while {
             b = (b * FACTOR_B) % LIMIT;
-        }
+            (b % 8) != 0
+        } {}
         if (a & 0xFFFF) == (b & 0xFFFF) {
             count = count + 1;
         }
