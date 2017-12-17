a576693a37488cd8b1495f19050a1f53da01eebc
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-17 08:39:56 +0000

    Solved 2017.17. Easy peasy.

--- src/m2017/problem_17.rs
+++ src/m2017/problem_17.rs
@@ -1,3 +1,24 @@
 pub fn run(_contents: &Vec<Vec<String>>) {
-    unimplemented!();
+    let input = 335;
+    let mut spinlocks: Vec<u32> = vec![0];
+    let mut position = 0usize;
+    for i in 1u32..2018u32 {
+        position = (position + input) % spinlocks.len();
+        spinlocks.insert(position + 1, i);
+        position = position + 1;
+    }
+    println!("Part1: {}", spinlocks[position + 1]);
+
+    let mut spinlocks_len = 1usize;
+    let mut position = 0usize;
+    let mut next = 0;
+    for i in 1u32..50_000_000u32 {
+        position = (position + input) % spinlocks_len;
+        if position == 0 {
+            next = i;
+        }
+        position = position + 1;
+        spinlocks_len = spinlocks_len + 1;
+    }
+    println!("Next: {}", next);
 }
