8542de356a57f60b9c5a6f1a50902320c88068fe
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-20 10:30:38 +0000

    Run in release mode.

--- run.sh
+++ run.sh
@@ -12,5 +12,5 @@ fi
 
 YEAR=$(date +%Y)
 DAY=$(date +%d)
-echo cargo run ${YEAR} ${DAY} ./inputs/${YEAR}_${DAY}${TEST}.txt
-RUST_BACKTRACE=1 cargo run ${YEAR} ${DAY} ./inputs/${YEAR}_${DAY}${TEST}.txt
+echo cargo run --release ${YEAR} ${DAY} ./inputs/${YEAR}_${DAY}${TEST}.txt
+RUST_BACKTRACE=1 cargo run --release ${YEAR} ${DAY} ./inputs/${YEAR}_${DAY}${TEST}.txt
