85ec8fb5247d91ffee54144cb0a49262d37ee8fb
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2017-12-20 17:50:44 +0000

    Better use of HashSet.

--- src/m2017/problem_4.rs
+++ src/m2017/problem_4.rs
@@ -4,10 +4,10 @@ pub fn run(contents: &[Vec<String>]) {
     let mut count = 0;
     for line in &contents[0] {
         let mut dup = false;
-        let mut hm: HashSet<String> = HashSet::new();
+        let mut set: HashSet<String> = HashSet::new();
         for word in line.split_whitespace() {
-            dup |= hm.contains(word);
-            hm.insert(word.into());
+            dup |= set.contains(word);
+            set.insert(word.into());
         }
         if !dup {
             count += 1;
@@ -17,13 +17,14 @@ pub fn run(contents: &[Vec<String>]) {
     count = 0;
     for line in &contents[0] {
         let mut dup = false;
-        let mut hm: HashSet<String> = HashSet::new();
+        let mut set: HashSet<String> = HashSet::new();
         for word in line.split_whitespace() {
             let mut chars = word.chars().collect::<Vec<_>>();
             chars.sort();
             let word: String = chars.iter().collect();
-            dup |= hm.contains(&word);
-            hm.insert(word);
+            if !set.insert(word) {
+                dup = true;
+            }
         }
         if !dup {
             count += 1;
